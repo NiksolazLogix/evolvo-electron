8e8e8378d0ef2860d3cd3f3c8d3c8fef6de21a77
Nicola Solazzo <solazzo@logixcorp.it>
2022-11-23 14:40:41 +0100

    standar nuxt 2 and electron

---
+++ client-app/components/Welcome.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="welcome">
+    <h3>Welcome on Electron + Nuxt 2</h3>
+  </div>
+</template>
+<style>
+  .welcome {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
---
+++ client-app/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <Welcome/>
+</template>
+
+<script>
+import Welcome from '~/components/Welcome.vue';
+
+export default {
+    name: "IndexPage",
+    components: { Welcome }
+}
+</script>
