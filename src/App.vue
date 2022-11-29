4221871672111b2050b33b97b8df070f71a43092
Nicola Solazzo <solazzo@logixcorp.it>
2022-11-29 18:31:16 +0100

    vue desktop app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,89 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="container">
+      <div class="text-center">
+        <h2 class="text-center mt-5 alert alert-dark">Trending Movies 🍿</h2>
+      </div>
+      <div class="text-center">
+        <div class="container">
+          <label for="toggle">
+            <button id="toggle" type="button" :class="'d-flex justify-content-center align-items-center mx-5 btn ' + online" @click="onToggle($event)">
+              <i class="fa fa-wifi" style="color: #fff;"></i>
+            </button>
+            {{onLine()}}
+          </label>
+        </div>
+      </div>
+      <div class="my-4">
+        <a href="#" @click="getTrendingMovies('day')" class="my-1 h4 btn btn-dark">
+          DAY</a
+        >
+        <a href="#" @click="getTrendingMovies('week')" class="mx-3 my-1 h4 btn btn-dark"
+          >WEEK</a
+        >
+      </div>
+
+      <div class="row" v-if="movies.length > 0">
+        <div class="col-md-3" v-for="(movie, i) in movies" :key="i">
+          <movie-card :movie="movie" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MovieCard from "./components/MovieCard.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MovieCard,
+  },
+  data() {
+    return {
+      connect: false,
+      online: 'btn-danger',
+      movies: [],
+      apiKey: "9637afe349c62f9da04ef69addf1442e",
+    };
+  },
+  methods: {
+    onToggle() {
+      this.connect = !this.connect
+      Navigator.onLine = this.connect
+      if(this.connect) {
+        this.online = 'btn-success'
+      } else {
+        this.online = 'btn-danger'
+      }
+    },
+    getTrendingMovies(category) {
+      return fetch(
+        `https://api.themoviedb.org/3/trending/movie/${category}?api_key=${this.apiKey}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          this.movies = data.results;
+        });
+    },
+    onLine() {
+      if(Navigator.onLine) {
+        return 'ONLINE'
+      } else {
+        return 'OFFLINE'
+      }
+    },
+  },
+  mounted() {
+    console.log(`welcome on ${process.env.APPPLICATION_NAME}`)
+    this.getTrendingMovies("day");
+  },
+};
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#toggle {
+  width: 30px;
+  height: 30px;
+  border-radius: 100px;
 }
 </style>
---
+++ src/components/MovieCard.vue
@@ -0,0 +1,50 @@
+<template>
+    <main>
+      <div class="card mb-4 shadow-lg border border-secondary bg-dark text-light" style="width: 18rem; height: 37rem;">
+        <img
+          class="card-img-top"
+          :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path"
+          alt="Card image cap"
+          height="500"
+        />
+        <div class="card-body">
+          <p class="card-text trucate">{{ movie.title }}</p>
+          <div class="">
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary">
+                  <i class="fas fa-heart"></i>
+                </button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">
+                  <i class="fas fa-plus"></i>
+                </button>
+              </div>
+              <small class="ms-auto text-muted"
+                >Rating {{ movie.vote_average }}/10</small
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </template>
+  <script>
+  export default {
+    name: "MovieCard",
+    props: {
+      movie: {
+        type: Object,
+        required: true,
+      },
+    },
+  };
+  </script>
+  <style>
+p {
+  width: 240px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
+  
